73e4ca8f0a6ce9c8ae7cff363516bb093c21be52
Christoph Büscher <christoph@elastic.co>
2016-08-17 14:00:30 +0200

    extend script to copy more documents

--- subsampleEnwiki.sh
+++ subsampleEnwiki.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-export es=localhost:9200
+export es=192.168.2.201:9200
 export index=enwiki
 export target=enwiki_rank
 
-curl -s -XDELETE $es/$target
+# clean target index in case it already exists
+echo "Removing old target index if it exists. Ignore errors if index didn't exist before"
+curl -s -XDELETE $es/$target?pretty
 
+# copy over settings and mappings from source index
 echo "copying source settings to target"
 curl -s -XGET $es/$index/_settings |
   jq --arg ind $index '{
@@ -20,6 +23,8 @@ curl -s -XGET $es/$index/_mapping |
   jq --arg ind $index .[$ind].mappings.page |
   curl -XPUT $es/$target/_mapping/page?pretty -d @-
 
+
+# get all articles that are rated in discernatron_ratings.tsv, lookup by title
 echo "retrieving all rated files from source index by title"
 cat discernatron_ratings.tsv|awk -F'\t' '{print $2}' > titles.txt
 [ -f bulk_index.txt ] && rm bulk_index.txt
@@ -37,11 +42,34 @@ do
   else
      echo "title [$title] not found"
   fi
-  #echo $source | curl -s -XPUT $es/$target/page/$id?pretty -d @-
 done <titles.txt
 
+# also add some article that are potentially not rated by using
+# the query string against the all field and copy top 10 hits
+echo "running all queries on _all field and get top 10"
+cat discernatron_ratings.tsv|awk -F'\t' '{print $1}' | sort | uniq > queries.txt 
+
+while read query;
+do
+  result=$(echo "{}" | 
+    jq --arg query "$query" '{ query: { match: { "all": $query } } }' |
+    curl -s -XGET $es/$index/page/_search?pretty -d @-)
+  hits=$(echo $result | jq ' .hits.hits | length')
+  if [ $hits != 0 ]; then
+    echo "Hits for [$query]: $hits"
+    for (( i=0; i <= $((hits-1)); ++i ))
+    do
+      #echo "$i"
+      id=$(echo $result | jq --arg i $i -r ' .hits.hits[$i|tonumber]._id')
+      source=$(echo $result | jq --arg i $i -c ' .hits.hits[$i|tonumber]._source')
+      echo '{"index" : { "_index" : "'$target'", "_type" : "page", "_id" : "'$id'" } }' >> bulk_index.txt
+      echo $source >> bulk_index.txt
+    done
+  else
+    echo "Nothing found for [$query]"
+  fi
+done <queries.txt
+
+# finally write everything in bulk
 echo "bulk indexing"
 curl -s -XPOST $es/$target/_bulk --data-binary "@bulk_index.txt";
-
-echo "running all queries on all field and get top 10"
-cat cat discernatron_ratings.tsv|awk -F'\t' '{print $1}' | sort | uniq > queries.txt 
