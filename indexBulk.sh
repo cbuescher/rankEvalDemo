c75905fdfe4ca0b3bbd8ce62e61de21bc08a7a26
Christoph Büscher <christoph@elastic.co>
2018-02-13 10:12:11 +0100

    Updating wikipedia data, settings and mappings

--- indexBulk.sh
+++ indexBulk.sh
@@ -5,16 +5,15 @@ export es_target=localhost:9200
 export target=enwiki_rank
 
 # clean target index in case it already exists
-echo "Removing old target index if it exists. Ignore errors if index didn't
-exist before"
+echo "Removing old target index if it exists. Ignore errors if index didn't exist before"
 curl -s -XDELETE $es_target/$target?pretty
 
 # copy over settings and mappings from source index
-echo "putting settings to target"
-cat orig_settings.txt | curl -XPUT -H 'Content-Type: application/json'  $es_target/$target?pretty -d @-
+echo "copy settings to $target"
+cat enwiki-settings.json | curl -XPUT -H 'Content-Type: application/json'  $es_target/$target?pretty -d @-
 
-echo "copying source mapping to target"
-cat corrected_mapping.txt | curl -XPUT -H 'Content-Type: application/json' $es_target/$target/_mapping/page?pretty -d @-
+echo "copy mapping to $target"
+cat enwiki-mapping.json | curl -XPUT -H 'Content-Type: application/json' $es_target/$target/_mapping/page?pretty -d @-
 
 # finally write everything in bulk
 echo "bulk indexing"
@@ -22,8 +21,9 @@ echo "bulk indexing"
 # chop down bulk file, might be too large otherwise
 for file in parts/bulkpart_*; do
   echo -n "${file}:  "
-  took=$(curl -s -XPOST -H 'Content-Type: application/json' $es_target/$target/_bulk?pretty --data-binary @$file |
-    grep took | cut -d':' -f 2 | cut -d',' -f 1)
+  result=$(curl -s -XPOST -H 'Content-Type: application/json' $es_target/$target/_bulk?pretty --data-binary @$file)
+  # echo $result > ${file}_bulklog.txt
+  took=$(echo $result |    grep took | cut -d':' -f 2 | cut -d',' -f 1)
   printf '%7s\n' $took
   [ "x$took" = "x" ] 
 done
---
+++ sampleEnwiki.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# change the following url and index names to reflect your local ES instance for the source index
+export es=localhost:9200
+export index=enwiki
+
+# get all articles that are rated in discernatron_ratings.tsv, lookup by title
+echo "retrieving all rated files from source index by title"
+cat discernatron_ratings.tsv|awk -F'\t' '{print $2}' > titles.txt
+[ -f bulk_index.txt ] && rm bulk_index.txt
+
+while read title; 
+do
+  result=$(echo "{}" | jq --arg titel "$title" '{ query: { bool: { should: [ {
+match: { "title.keyword": $titel } }, { match: { "redirect.title.keyword":
+$titel } } ] } } }' | curl -s -XGET -H'Content-Type: application/json' $es/$index/page/_search?pretty -d @-
+  )
+  id=$(echo $result | jq -r .hits.hits[0]._id)
+  source=$(echo $result | jq -c .hits.hits[0]._source)
+  if [ $id != null ]; then
+     echo '{"index" : { "_type" : "page", "_id" : "'$id'" } }' >> bulk_index.txt
+     echo $source >> bulk_index.txt
+  else
+     echo "title [$title] not found"
+  fi
+done <titles.txt
+
+# also add some article that are potentially not rated by using
+# the query string against the all field and copy top 20 hits
+echo "running all queries on _all field and get top 20"
+cat discernatron_ratings.tsv|awk -F'\t' '{print $1}' | sort | uniq > queries.txt 
+
+while read query;
+do
+  result=$(echo "{}" | 
+    jq --arg query "$query" '{ query: { match: { "all": $query } } , "size" : 20 }' |
+    curl -s -XGET  -H'Content-Type: application/json' $es/$index/page/_search?pretty -d @-)
+  #echo $result
+  hits=$(echo $result | jq ' .hits.hits | length')
+  if [ $hits != 0 ]; then
+    echo "Hits for [$query]: $hits"
+    for (( i=0; i <= $((hits-1)); ++i ))
+    do
+      id=$(echo $result | jq --arg i $i -r ' .hits.hits[$i|tonumber]._id')
+      source=$(echo $result | jq --arg i $i -c ' .hits.hits[$i|tonumber]._source')
+      echo '{"index" : { "_type" : "page", "_id" : "'$id'" } }' >> bulk_index.txt
+      echo $source >> bulk_index.txt
+    done
+  else
+    echo "Nothing found for [$query]"
+  fi
+done < queries.txt
+
+# split the large bulk_index.txt into parts 
+split -a 3 -l 500 bulk_index.txt bulkpart_
+mv bulkpart_* parts
